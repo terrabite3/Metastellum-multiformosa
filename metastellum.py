eeb56adea415d2462885291595d8fd4f158a4768
terrabite3 <terrabite3@gmail.com>
2023-04-30 11:31:19 -0700

    Modifications for creating a video

--- metastellum.py
+++ metastellum.py
@@ -5,11 +5,13 @@ import math
 import cairo
 import subprocess
 import os.path
-from numpy import arange
+from numpy import arange, linspace
 import colorsys
 import argparse
 import time
 import glob
+
+import multiprocessing
 
 def circle_point(theta):
     x = math.sin(2 * math.pi * theta) / 2
@@ -117,32 +119,58 @@ def get_epoch(filename):
 
 
 
+def render_frame(time):
+    # delta_time = (float(args.time) + i * float(args.time_step)) * unit_time
 
+    frame_num = time + 1
+    # if verbose:
+    #     print(frame_num)
+
+    filename = make_name(frame_num, prefix='frames/chord', suffix='.png', decimal_digits=5, total_digits=8)
+
+    if os.path.exists(filename):
+        return
+
+    draw_frame(filename, frame_num, num_lines, size, thinness=5000)
+    # if verbose:
+    #     print(filename)
+
+# The number of lines to draw
+num_lines = 2000 
+
+# The number of seconds to take to advance by "1"
+unit_time = 60 * 60 * 24   # 1 day
+
+size = 2160
 
 if __name__ == '__main__':
 
 
     parser = argparse.ArgumentParser(description='Draw a frame of Metastellum multiformosa')
 
-    parser.add_argument('-s', '--size', dest='size', action='store', default=0, help='width and height of image')
+    # parser.add_argument('-s', '--size', dest='size', action='store', default=0, help='width and height of image')
     parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('-t', '--time', dest='time', action='store')
+    # parser.add_argument('--time-step', dest='time_step', action='store')
+    parser.add_argument('-e', '--end-time', dest='end_time', action='store')
+    parser.add_argument('-n', dest='num_frames', action='store', default=1)
     args = parser.parse_args()
 
     verbose = args.verbose
 
-    size = args.size
-    if size == 0:
-        with open('/sys/class/graphics/fb0/virtual_size', 'r') as fb_size:
-            sizes = fb_size.readline().split(',')
-            size = min(int(sizes[0]), int(sizes[1]))
-            if verbose: 
-                print('Detected size ' + str(size))
-            # Double the size for anti-aliasing
-            size *= 2
+    # size = args.size
+    # if size == 0:
+    #     with open('/sys/class/graphics/fb0/virtual_size', 'r') as fb_size:
+    #         sizes = fb_size.readline().split(',')
+    #         size = min(int(sizes[0]), int(sizes[1]))
+    #         if verbose: 
+    #             print('Detected size ' + str(size))
+    #         # Double the size for anti-aliasing
+    #         size *= 2
 
 
             
-    epoch = get_epoch('epoch.txt')
+    # epoch = get_epoch('epoch.txt')
 
 
 
@@ -157,42 +185,26 @@ if __name__ == '__main__':
     # Ctrl+C stops the image viewer, but not the script.
     # After writing each frame, check that the image view is running.
     # If it isn't running for three consecutive frames, end the script.
-    strikes_left = 3
-
-    while True:
-
-        current_time = time.time()
-        delta_time = current_time - epoch
+    # strikes_left = 3
 
 
-        frame_num = delta_time / unit_time + 1
-        if verbose:
-            print(frame_num)
 
-        filename = make_name(frame_num, prefix='frames/chord', suffix='.png', decimal_digits=5, total_digits=8)
+    # for i in range(int(args.num_frames)):
 
-        draw_frame(filename, frame_num, num_lines, size, thinness=5000, verbose=verbose)
-        if verbose:
-            print(filename)
 
-        subprocess.run(['ln', '-s', '-f', filename, 'link1.png'])
-        subprocess.run(['ln', '-s', '-f', filename, 'link2.png'])
-        subprocess.run(['ln', '-s', '-f', filename, 'link3.png'])
 
-        # Remove previous frames so the disk doesn't fill up
-        for old_file in glob.glob('frames/chord*.png'):
-            if old_file != filename:
-                os.remove(old_file)
+    start_time = float(args.time)
+    end_time = float(args.end_time)
+    num_frames = int(args.num_frames)
 
-        # Check if the image viewer is running
-        ps = subprocess.Popen('ps -e | grep fbi', shell=True, stdout=subprocess.PIPE)
-        output = ps.stdout.read()
-        ps.stdout.close()
-        ps.wait()
-        if len(output) == 0:
-            strikes_left -= 1
-            if strikes_left == 0:
-                print('fbi is not running; exiting')
-                break
-        else:
-            strikes_left = 3
+    pool = multiprocessing.Pool(16)
+    pool.map(render_frame, linspace(start_time, end_time, num_frames, endpoint=False))
+
+
+# 60 fps
+# 30 seconds per "1"
+# 1800 frames per "1"
+# Song is 30 minutes long
+# 54000 frames total
+
+# No, I think 30 seconds per "1" is too fast. 60 is better
